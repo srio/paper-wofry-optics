894b8d6b90267676576abd6581a9d98beb7d5e81
Manuel Sanchez del Rio <srio@esrf.eu>
2024-10-02 10:58:47 +0200

    VERSION SUBMITTED TO ArXiv

--- mkarxiv.sh
+++ mkarxiv.sh
@@ -8,26 +8,49 @@ mkdir ARXIV
 cd ARXIV
 
 mkdir figures
-cp ../figures/*.png figures
-cp ../figures/*.pdf figures
-cp ../main.tex .
+
+cp ../figures/CFvsEmittance.pdf figures
+cp ../figures/CFvsGap.pdf figures
+cp ../figures/CMD_H.png figures
+cp ../figures/CMD_H-propagated.png figures
+cp ../figures/CMD_V.png figures
+cp ../figures/CMD_V-propagated.png figures
+cp ../figures/croppingslit.png figures
+cp ../figures/diffractionlimited.png figures
+cp ../figures/GSM2D.png figures
+cp ../figures/GSM_H.png figures
+cp ../figures/GSM_H-propagated.png figures
+cp ../figures/GSM_V.png figures
+cp ../figures/GSM_V-propagated.png figures
+cp ../figures/oasyselements.png figures
+cp ../figures/oasyssources.png figures
+cp ../figures/SD_H.png figures
+cp ../figures/SD_H-propagated.png figures
+cp ../figures/SD_V.png figures
+cp ../figures/SD_V-propagated.png figures
+
+#cp ../figures/*.png figures
+#cp ../figures/*.pdf figures
+
+cp ../template.tex .
+cp ../iopconfser.cls .
 
 
 cp ../iucr.bib .
 cp ../iucr.bst .
 cp ../iucr.cls .
 
-rm main.pdf 
+rm template.pdf 
 
-pdflatex main.tex
-bibtex main
+pdflatex template.tex
+bibtex template
 sleep 1.0
-pdflatex main.tex
-pdflatex main.tex
+pdflatex template.tex
+pdflatex template.tex
 
-okular main.pdf
+okular template.pdf
 
 sleep 1.0
-rm main.aux main.blg main.log main.pdf
+rm template.aux template.blg template.log template.pdf
 cd ..
 zip -r ARXIV.zip ARXIV
